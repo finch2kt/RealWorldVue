7a44e022a71707628a238626e940b491abbb678c
Kashaun Finch <kashaunt.finch@gmail.com>
2019-09-30 05:10:19 -0400

    Global Components Lession

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="icon-wrapper">
+    <svg class="icon" width height>
+      <use v-bind="{ 'xlink:href': '/feather-sprite.svg#' + name }" />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    width: {
+      type: [Number, String],
+      default: 24
+    },
+    hieght: {
+      type: [Number, String],
+      default: 24
+    }
+  }
+};
+</script>
+
+<style scoped></style>
